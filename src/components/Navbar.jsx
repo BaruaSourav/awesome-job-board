c06c46ccb307980a09cd858600c24357543715e0
Sourav Barua <sourav.shr6@gmail.com>
2024-12-31 00:06:09 -0500

    Components are separated phase 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,50 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/images/AJB-logo.svg';
+
 
 const Navbar = () => {
-  return (
-    <>
-        <nav className="bg-indigo-700 border-b border-indigo-500">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="flex h-20 items-center justify-between">
-            <div
-              className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-            >
-              {/* <!-- Logo --> */}
-              <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-                <img
-                  className="h-10 w-auto"
-                  src="AJB-logo.png"
-                  alt="Awesome Job Board Logo"
-                />
-                <span className="hidden md:block text-white text-2xl font-bold ml-2">Awesome Jobs</span>
-              </a>
-              <div className="md:ml-auto">
-                <div className="flex space-x-2">
-                  <a
-                    href="/index.html"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                  >
-                  <a
-                    href="/jobs.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                  >
-                  <a
-                    href="/add-job.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                  >
+    return (
+        <>
+            <nav className="bg-teal-800 border-b border-teal-700">
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className="flex h-20 items-center justify-between">
+                        <div
+                            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+                        >
+                            {/* <!-- Logo --> */}
+                            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+                                <img
+                                    className="h-10 w-auto"
+                                    src={logo}
+                                    alt="Salary Sense Logo"
+                                />
+                                <span className="hidden md:block text-white text-2xl font-bold ml-2">SalarySense</span>
+                            </a>
+                            <div className="md:ml-auto">
+                                <div className="flex space-x-2">
+                                    <a
+                                        href="/index.html"
+                                        className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    >Home</a
+                                    >
+                                    <a
+                                        href="/jobs.html"
+                                        className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    >Salaries</a
+                                    >
+                                    <a
+                                        href="/add-job.html"
+                                        className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    >Add Salary Anonymously</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  )
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
